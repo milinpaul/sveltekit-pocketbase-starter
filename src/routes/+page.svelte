3bdcce6265a1369e4777d69537b426370f24534f
Milin Paul <milin.seby@gmail.com>
2024-07-03 16:17:02 +0530

    basic barebone sveltekit and added shadcn ui

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	import Button from "$lib/components/ui/button/button.svelte";
+
+</script>
+<h1>Welcome to SvelteKit</h1>
+<Button on:click={() => console.log('Clicked me')}>Click Me!</Button>
