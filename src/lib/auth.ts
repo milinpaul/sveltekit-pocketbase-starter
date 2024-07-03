b3541cb867f4d6f168486b84658a8aa6524e297a
Milin Paul <milin.seby@gmail.com>
2024-07-03 19:10:46 +0530

    added authjs prisma schema and its adapter.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,9 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+
+import prisma from './prisma';
 
 export const { handle } = SvelteKitAuth({
+	adapter: PrismaAdapter(prisma),
 	providers: []
 });
