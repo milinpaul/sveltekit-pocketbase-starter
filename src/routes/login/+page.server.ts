113a45a4f56395fdf53c04c2935771c5b252ce4b
Milin Paul <milin.seby@gmail.com>
2024-07-25 10:14:54 +0530

    added env sample and removed some unused code

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,10 +1,5 @@
-import type { PageServerLoad } from './$types';
+// import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ locals }) => {
-	const session = await locals.auth();
-
-	console.log('session', session);
-	return {
-		session
-	};
-};
+// export const load: PageServerLoad = async ({ locals }) => {
+	 
+// };
