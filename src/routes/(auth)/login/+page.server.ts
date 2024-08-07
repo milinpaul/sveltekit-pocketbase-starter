aa80081b8e67664292a1e27a40b9ff11d35dde21
Milin Paul <milin.seby@gmail.com>
2024-08-07 11:01:06 +0530

    added validation for login form

--- src/routes/(auth)/login/+page.server.ts
+++ src/routes/(auth)/login/+page.server.ts
@@ -1,7 +1,44 @@
-import type { PageServerLoad } from './$types';
+import { z } from 'zod';
+import { fail } from '@sveltejs/kit';
+
+import type { PageServerLoad, Actions } from './$types';
+
+const loginSchema = z.object({
+	email: z
+		.string({ required_error: 'Email is required' })
+		.min(1, { message: 'Email is required' })
+		.email({ message: 'Email must be a valid email address' }),
+	password: z
+		.string({ required_error: 'Password is required' })
+		.min(6, { message: 'Password must be at least 6 characters' })
+		.max(32, { message: 'Password must be less than 32 characters' })
+		.trim()
+});
 
 export const load: PageServerLoad = async () => {
 	return {
 		title: 'Login'
 	};
 };
+
+export const actions = {
+	login: async ({ request }) => {
+		const formData = Object.fromEntries([...(await request.formData())]);
+
+		try {
+			const result = loginSchema.parse(formData);
+			console.log('result', result);
+		} catch (error) {
+			console.log('error', error);
+			const { password, ...rest } = formData;
+
+			if (error instanceof z.ZodError) {
+				const { fieldErrors: errors } = error.flatten();
+				return fail(422, {
+					data: rest,
+					errors
+				});
+			}
+		}
+	}
+} satisfies Actions;
--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,39 +1,72 @@
 <script lang="ts">
-  import { Button } from "$lib/components/ui/button";
-  import { Input } from "$lib/components/ui/input";
-  import { Label } from "$lib/components/ui/label";
+	import { LoaderCircle } from 'lucide-svelte';
+
+	import { enhance } from '$app/forms';
+	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
+	import { Label } from '$lib/components/ui/label';
+
+	export let form;
+	let formLoading = false;
 </script>
 
-<div class="w-full h-screen flex items-center justify-center px-4">
-  <div class="flex items-center justify-center py-12">
-    <div class="mx-auto grid w-[350px] gap-6">
-      <div class="grid gap-2 text-center">
-        <h1 class="text-3xl font-bold">Login</h1>
-        <p class="text-muted-foreground text-balance">
-          Enter your email below to login to your account
-        </p>
-      </div>
-      <div class="grid gap-4">
-        <div class="grid gap-2">
-          <Label for="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" required autofocus />
-        </div>
-        <div class="grid gap-2">
-          <div class="flex items-center">
-            <Label for="password">Password</Label>
-            <a href="##" class="ml-auto inline-block text-sm underline">
-              Forgot your password?
-            </a>
-          </div>
-          <Input id="password" type="password" required />
-        </div>
-        <Button type="submit" class="w-full">Login</Button>
-        <Button variant="outline" class="w-full">Login with Google</Button>
-      </div>
-      <div class="mt-4 text-center text-sm">
-        Don&apos;t have an account?
-        <a href="/register" class="underline"> Register </a>
-      </div>
-    </div>
-  </div>
+<div class="flex h-screen w-full items-center justify-center px-4">
+	<div class="flex items-center justify-center py-12">
+		<div class="mx-auto grid w-[350px] gap-6">
+			<div class="grid gap-2 text-center">
+				<h1 class="text-3xl font-bold">Login</h1>
+				<p class="text-balance text-muted-foreground">
+					Enter your email below to login to your account
+				</p>
+			</div>
+			<form
+				class="grid gap-4"
+				method="POST"
+				action="?/login"
+				use:enhance={() => {
+					formLoading = true;
+					return async ({ update }) => {
+						await update();
+						formLoading = false;
+					};
+				}}
+			>
+				<div class="grid gap-2">
+					<Label for="email">Email</Label>
+					<Input
+						id="email"
+						type="email"
+						name="email"
+						placeholder="m@example.com"
+						value={form?.data?.email ?? ''}
+						autofocus
+					/>
+					{#if form?.errors?.email}
+						<span class="text-sm text-red-500">{form?.errors?.email[0]}</span>
+					{/if}
+				</div>
+				<div class="grid gap-2">
+					<div class="flex items-center">
+						<Label for="password">Password</Label>
+						<a href="##" class="ml-auto inline-block text-sm underline"> Forgot your password? </a>
+					</div>
+					<Input id="password" type="password" name="password" />
+					{#if form?.errors?.password}
+						<span class="text-sm text-red-500">{form?.errors?.password[0]}</span>
+					{/if}
+				</div>
+				<Button type="submit" class="w-full" disabled={formLoading}>
+					{#if formLoading}
+						<LoaderCircle class="mr-2 h-4 w-4 animate-spin" />
+					{/if}
+					Login
+				</Button>
+				<Button variant="outline" class="w-full">Login with Google</Button>
+			</form>
+			<div class="mt-4 text-center text-sm">
+				Don't have an account?
+				<a href="/register" class="underline"> Register </a>
+			</div>
+		</div>
+	</div>
 </div>
