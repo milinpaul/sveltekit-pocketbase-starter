9def3981700f00b4150fa9581cbbee0bd03be595
Milin Paul <milin.seby@gmail.com>
2024-11-15 16:05:43 +0530

    upgraded svelte and sveltekit to the latest, added default template for home page

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import SidebarPage from '$lib/components/sidebar-page.svelte';
 	import Button from '$lib/components/ui/button/button.svelte';
 
 	/** @type {import('./$types').PageData} */
@@ -8,7 +9,8 @@
 <div>
 	<h1>Dashboard</h1>
 	<h2>{data.user.username}</h2>
-	<form action="/logout" method="POST">
+	<!-- <form action="/logout" method="POST">
 		<Button variant="link" type="submit">SignOut</Button>
-	</form>
+	</form> -->
+	<SidebarPage />
 </div>
